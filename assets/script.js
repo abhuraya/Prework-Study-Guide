471ec066b2d5c2da7d907ba482dfcd8aaeaa923c
abhuraya <149016492+abhuraya@users.noreply.github.com>
2023-12-14 18:15:53 -0500

    Update Java

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+/*
 if (topic === 'HTML') {
     console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -11,18 +12,10 @@ if (topic === 'HTML') {
 } else {
     console.log('Please try again!');
 }
-
-var shapes = ["star", "square", "pentagon", "hexagon"];
-
-for(var x=0; x < shapes.length; x++){
-    console.log(topics[x]);
-
-/*
-console.log(shapes[0]);
-console.log(shapes[1]);
-console.log(shapes[2]);
-console.log(shapes[3]);
 */
 
 
+
+for(var x=0; x < topics.length; x++){
+    console.log(topics[x]);
 }
